92c45f06c64f8f96bc91884c7d0b552fed525eae
Josephine Ortgies <ortgies@uni-bremen.de>
2017-11-30 13:07:31 +0100

    UI

---
+++ Robota/Assets/Scripts/OpenMainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMainMenu : MonoBehaviour {
+
+ 
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var gameObject = GameObject.Find("GameObject"); // The parent game object to toggle
+            gameObject.SetActive(true); //true or false
+        }
+    }
+}
