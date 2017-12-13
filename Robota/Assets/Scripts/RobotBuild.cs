f8063a4b708563e09d27579305c378338903a83e
Ivan Latták <ivanlattak@gmail.com>
2017-12-13 22:56:37 +0100

    Started working again on the work I lost, concerning background logic for creating Robot and associated variables

---
+++ Robota/Assets/Scripts/RobotBuild.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBuild : MonoBehaviour {
+
+    public GameObject head;
+    public GameObject torso;
+    public GameObject arms;
+    public GameObject legs;
+
+    public Sprite blankSprite;
+
+    private bool RobotFullyBuilt()
+    {
+        GameObject[] parts = { head, torso, arms, legs };
+        return true;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
