e5cbd1376aaccd1708abe6f1fc5408bbe21aec6a
Josephine Ortgies <ortgies@uni-bremen.de>
2018-01-16 22:32:31 +0100

    Audio management, sliders in Options Menu work now, more sounds added

---
+++ Robota/Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeManager : MonoBehaviour {
+
+    public AudioMixerGroup soundEffects;
+    public AudioMixerGroup backgroundMusic;
+
+    public void SetVolumeSounds(float s){
+        soundEffects.audioMixer.SetFloat("soundEffects", s);
+
+    }
+
+    public void SetBackgroundSounds(float s)
+    {
+        soundEffects.audioMixer.SetFloat("background", s);
+
+    }
+   
+}
